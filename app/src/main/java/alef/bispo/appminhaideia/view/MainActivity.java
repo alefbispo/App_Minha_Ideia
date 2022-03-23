e7e7393219f52ad5920a338076b36662cf6c5d94
Alef Bispo <alef.b.dos.santos@hotmail.com>
2022-03-22 21:30:35 -0300

    Usando Bundle para passar informações de uma actinity para a outra

--- app/src/main/java/alef/bispo/appminhaideia/view/MainActivity.java
+++ app/src/main/java/alef/bispo/appminhaideia/view/MainActivity.java
@@ -3,14 +3,25 @@ package alef.bispo.appminhaideia.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import alef.bispo.appminhaideia.R;
+import alef.bispo.appminhaideia.model.Cliente;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "APP_MINHA_IDEIA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Tela Principal carregada...");
+
+        Bundle bundle = getIntent().getExtras();
+        Log.d(TAG, "onCreate: Nome..."+bundle.getString("Nome"));
+        Log.d(TAG, "onCreate: Email..."+bundle.getString("Email"));
+
     }
 }
--- app/src/main/java/alef/bispo/appminhaideia/view/Splash.java
+++ app/src/main/java/alef/bispo/appminhaideia/view/Splash.java
@@ -8,18 +8,21 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import alef.bispo.appminhaideia.R;
+import alef.bispo.appminhaideia.model.Cliente;
 
 public class Splash extends AppCompatActivity {
 
 
     String TAG = "APP_MINHA_IDEIA";
     int tempoDeEspera = 1000 * 5;
+    Cliente objCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Log.d(TAG, "Tela Splash Carregrando...");
+
         trocarTela();
     }
 
@@ -28,7 +31,23 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
+                objCliente = new Cliente("Alef",
+                        "teste@gmail.com",
+                        "999461376",
+                        28,
+                        true);
+
+                Log.d(TAG, "TrocarTela: Mudando de tela...");
+
                 Intent trocarDeTela = new Intent(Splash.this, MainActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("Nome",objCliente.getNome());
+                bundle.putString("Email",objCliente.getEmail());
+
+                trocarDeTela.putExtras(bundle);
+
                 startActivity(trocarDeTela);
                 finish();
             }
